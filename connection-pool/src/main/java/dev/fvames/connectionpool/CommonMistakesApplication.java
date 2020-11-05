df4abc95167e01b02c8abe467b8ecfac3ae7ccb8
李圣洁 <op_lisj@essence.com.cn>
2020-11-05 18:11:26 +0800

    connection pool

--- connection-pool/src/main/java/dev/fvames/connectionpool/CommonMistakesApplication.java
+++ connection-pool/src/main/java/dev/fvames/connectionpool/CommonMistakesApplication.java
@@ -1,4 +1,4 @@
-package dev.fvames.connectionpool.jedis;
+package dev.fvames.connectionpool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
