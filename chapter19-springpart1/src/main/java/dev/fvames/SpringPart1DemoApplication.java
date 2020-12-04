3e2615d69af9bc2047b77091feeaae0ac0c80d83
Fvames <warmhhands@gmail.com>
2020-12-04 17:49:44 +0800

    advanced features

---
+++ chapter19-springpart1/src/main/java/dev/fvames/SpringPart1DemoApplication.java
@@ -0,0 +1,13 @@
+package dev.fvames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPart1DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPart1DemoApplication.class, args);
+    }
+}
+
